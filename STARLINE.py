ca91dec50e9b0ba324bc4ba125af9da44cac07a9
Fernando <fmunoz@us.es>
2018-07-12 21:27:47 +0200

    creada la estructura de STARLINE.py

--- STARLINE.py
+++ STARLINE.py
@@ -23,25 +23,118 @@ configFile = open('assets/config/config.csv')
 configReader = csv.reader(configFile, delimiter=';')
 configList = list(configReader)
 
-
-
-
-
+spacePressed = False
+mousePressed = False
 
 
 # CONSTANTS
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 700
-
-# LOAD IMAGES
-
-# FUNCTIONS
+FPS = 60
 
 # PYGAME OBJECTS
 
 pygame.init()
 surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('STARLINE')
+clock = pygame.time.Clock()
+pygame.font.init()
+textFont = pygame.font.SysFont("Becker", 40)
+
+# LOAD IMAGES
+
+
+# FUNCTIONS
+
+def quitGame():
+    cofigFile.close()
+    pygame.quit()
+    sys.exit()
+    
+def drawStage():
+    global surface
+    surface.fill((0,0,0))
+
+def welcomeScreen():
+    global surface
+    renderedText = textFont.render('Pantalla de inicio. Pulsa espacio o presiona el ratón', 1, (255,255,255))
+    surface.blit(renderedText, (100, 100))
+    
+def levelSelector():
+    global surface
+    renderedText = textFont.render('Selector de nivel. Presiona el ratón', 1, (255,255,255))
+    surface.blit(renderedText, (100, 100))
+
+def startAnimation():
+    global surface
+    renderedText = textFont.render('Animacion de inicio de nivel. Presiona el ratón', 1, (255,255,255))
+    surface.blit(renderedText, (100, 100))
+    
+def story():
+    global surface
+    renderedText = textFont.render('Diálogos. Presiona el ratón', 1, (255,255,255))
+    surface.blit(renderedText, (100, 100))
+    
+def inGame():
+    global surface
+    renderedText = textFont.render('Jugando... Presiona el ratón', 1, (255,255,255))
+    surface.blit(renderedText, (100, 100))
 
 # MAIN LOOP
+
+while True:
+    drawStage()
+    # Handle user and system events 
+    for event in GAME_EVENTS.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE :
+                quitGame()
+            if event.key == pygame.K_SPACE :
+                spacePressed = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mousePressed = True
+        if event.type == GAME_GLOBALS.QUIT:
+            quitGame()
+
+    if state == 0 :
+        welcomeScreen()
+        if spacePressed or mousePressed:
+            state += 1
+            spacePressed = False
+            mousePressed = False
+            
+    if state == 1:
+        levelSelector()
+        if spacePressed or mousePressed:
+            state += 1
+            spacePressed = False
+            mousePressed = False
+            
+    if state == 2:
+        startAnimation()
+        if spacePressed or mousePressed:
+            state += 1
+            spacePressed = False
+            mousePressed = False
+    
+    if state == 3:
+        story()
+        if spacePressed or mousePressed:
+            state += 1
+            spacePressed = False
+            mousePressed = False
+    
+    if state == 4:
+        inGame()
+        if spacePressed or mousePressed:
+            state = 1
+            spacePressed = False
+            mousePressed = False
+    
+    
+    clock.tick(FPS)
+    pygame.display.update()
+    
+
+        
